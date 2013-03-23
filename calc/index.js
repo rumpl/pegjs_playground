a515895646cde9d9725df0bc0fc73eeec33d8180
rumpl <lukic.djordje@gmail.com>
2013-03-23 10:20:08 +0100

    Small modifications

--- calc/index.js
+++ calc/index.js
@@ -1,26 +1,28 @@
-var parser = require('./calc.js');
+var parser = require('./calc');
 
 var ast = parser.parse('-2*3.23');
 
 var expr = function (ex) {
-    if (ex.type === "num") {
-        return ex.val;
-    }
+  if (ex.type === "num") {
+    return ex.val;
+  }
 
-    return call(ex);
+  return call(ex);
 };
 
 var call = function (node) {
-    switch(node.op) {
-        case '+':
-            return expr(node.left) + expr(node.right);
-        case '-':
-            return expr(node.left) - expr(node.right);
-        case '*':
-            return expr(node.left) * expr(node.right);
-        case '/':
-            return expr(node.left) / expr(node.right);
-    }
+  switch (node.op) {
+    case '+':
+      return expr(node.left) + expr(node.right);
+    case '-':
+      return expr(node.left) - expr(node.right);
+    case '*':
+      return expr(node.left) * expr(node.right);
+    case '/':
+      return expr(node.left) / expr(node.right);
+  }
+
+  throw new Error('Unknown operator ' + node.op);
 };
 
 console.log(call(ast));
